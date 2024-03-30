1b190ef76aab1d378ac2e2bd19902bedccd4995f
Piush Singh <101204142+SPYDPiush@users.noreply.github.com>
2024-03-30 08:29:06 +0000

    Number and math properties use

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
  console.log('hello')
+
+
+//  conversion
+
+let a ="29bh";
+
+console.log( typeof a)
+
+console.log(typeof(Number(a)))
+console.log(Number(a))
+
+
+console.log(Math.random())
+
+console.log(Math.floor(Math.random()*10+1))
+
+// formula for random number calculate even when max and min value provided
+
+let max=20
+let min = 10
+
+console.log(Math.floor(Math.random() * (max-min +1))+min)
+
